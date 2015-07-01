93509633a641b8d4ca399a47f49d697bdced1953
rolaveric <rolaveric@gmail.com>
2015-07-01 22:04:23 +1000

    Added Typescript support.
    Added support for Typescript as a transpiler.
    Also allowed for any future transpilers to be supported without changes.
    Added deprecation warning when looking up module paths using `require.resolve()`.
    Fixes #24 and #25

--- test/index.spec.js
+++ test/index.spec.js
@@ -28,6 +28,15 @@ describe('initSystemJs', function() {
     expect(config.files[3].pattern).toMatch(/\/system\.src\.js$/);
   });
 
+  it('Adds Typescript instead of Traceur if the transpiler option is set', function() {
+    config.systemjs.config = {transpiler: 'typescript'};
+    initSystemJs(config);
+    expect(config.files[0].pattern).toMatch(/\/typescript\/.*?\/typescript\.js$/);
+    expect(config.files[1].pattern).toMatch(/\/es6-module-loader\.src\.js$/);
+    expect(config.files[2].pattern).toMatch(/\/system-polyfills\.js$/);
+    expect(config.files[3].pattern).toMatch(/\/system\.src\.js$/);
+  });
+
   it('Omits adding a file pattern for a transpiler if the transpiler option is set to null', function() {
     config.systemjs.config = {transpiler: null};
     initSystemJs(config);
